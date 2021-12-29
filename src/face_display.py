5986201edd965dc30f7153475bb56b9a3518cfa6
jmfaulk <jmfaulk@protonmail.com>
2021-12-29 16:39:22 -0500

    figuring out packages in python. Imports will now work across directories.

--- src/face_display.py
+++ src/face_display.py
@@ -1,12 +1,12 @@
+import sys
+print(sys.path)
 import cv2
-
-path = '../resources/bmo-imgs/bmo9.jpg'
+import graphics.faces as face
+# path = '../resources/bmo-imgs/bmo9.jpg'
 # list of bmo faces and expression equivalent
-happy = cv2.imread(path)
+happy = cv2.imread(face.happy)
 cv2.imshow('happy', happy)
-
+#
 cv2.waitKey(0)
-
+#
 cv2.destroyAllWindows()
-
-
