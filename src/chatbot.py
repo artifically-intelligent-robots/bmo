965f14acc185beafdcef26774dfbadb464b24060
jmfaulk <jmfaulk@protonmail.com>
2022-01-08 09:38:28 -0500

    adding neural network to chatbot

--- src/chatbot.py
+++ src/chatbot.py
@@ -4,7 +4,7 @@ stemmer = LancasterStemmer
 
 import numpy as np
 import tflearn
-import tensorflow
+import tensorflow as tf
 import random
 import json
 
@@ -26,7 +26,7 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         tokens = nltk.word_tokenize(pattern)
         words.extend(tokens)
-        pattern_x.append(pattern)
+        pattern_x.append(token)
         tag_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
@@ -34,24 +34,23 @@ for intent in data["intents"]:
 }
 
 # stem all words
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
 
 #remove duplicate words, convert back into list, sort it alphabetic
 words = sorted(list(set(words)))
-
 
 # empty list size of labels
 out_empty = [0 for _ in range(len(labels))]
 
 # one hot encoded words in patterns_x
 # create a bag of words
-# doc = string phrase
 for i, pattern in enumerate(pattern_x):
     bag = []
 
     # all stemmed words in THIS pattern
     tokens = [stemmer.stem(w) for w in pattern]
 
+    # 1 = word exists, 0 = word does not exist in pattern
     for w in words:
         if w in tokens:
             bag.append(1)
@@ -66,6 +65,7 @@ for i, pattern in enumerate(pattern_x):
     # tag_y[1] = Goodbye
     # labels.index(goodbye) = 3 ; labels [greeting, anger, sad, goodbye]
     # output_row[3] = 1
+    # this tells which tag correlates with pattern
     output_row[labels.index(tag_y[i])] = 1
 
     training.append(bag)
@@ -73,6 +73,22 @@ for i, pattern in enumerate(pattern_x):
 
 training = np.array(training)
 output = np.array(output)
+''' Preprocessing complete '''
+
+
+tf.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0])), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
+
 
 
 
