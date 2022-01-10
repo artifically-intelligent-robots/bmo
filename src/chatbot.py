d5de132c9843c4ea604d8910c482a42cd5fb39b0
jmfaulk <jmfaulk@protonmail.com>
2022-01-09 21:09:39 -0500

    addd try except to chatbot

--- src/chatbot.py
+++ src/chatbot.py
@@ -11,71 +11,74 @@ import json
 
 with open("quotes.json") as file:
     data = json.load(file)
+try:
 
-# all stemmed words in all patterns
-words = []
-# all tag labels
-labels = []
-# list of all the different patterns
-pattern_x = []
-# corresponding 'tag' label (to pattern_x)
-tag_y = []
+except:
 
-#load in json file data
-for intent in data["intents"]:
-    # for each input pattern
-    for pattern in intent["patterns"]:
-        tokens = nltk.word_tokenize(pattern)
-        words.extend(tokens)
-        pattern_x.append(tokens)
-        tag_y.append(intent["tag"])
+    # all stemmed words in all patterns
+    words = []
+    # all tag labels
+    labels = []
+    # list of all the different patterns
+    pattern_x = []
+    # corresponding 'tag' label (to pattern_x)
+    tag_y = []
 
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
+    #load in json file data
+    for intent in data["intents"]:
+        # for each input pattern
+        for pattern in intent["patterns"]:
+            tokens = nltk.word_tokenize(pattern)
+            words.extend(tokens)
+            pattern_x.append(tokens)
+            tag_y.append(intent["tag"])
 
-# stem all words
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
-#remove duplicate words, convert back into list, sort it alphabetic
-words = sorted(list(set(words)))
+    # stem all words
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+
+    #remove duplicate words, convert back into list, sort it alphabetic
+    words = sorted(list(set(words)))
 
 
-training = []
-output = []
-# empty list size of labels
-out_empty = [0 for _ in range(len(labels))]
+    training = []
+    output = []
+    # empty list size of labels
+    out_empty = [0 for _ in range(len(labels))]
 
-# one hot encoded words in patterns_x
-# create a bag of words
-for i, pattern in enumerate(pattern_x):
-    bag = []
+    # one hot encoded words in patterns_x
+    # create a bag of words
+    for i, pattern in enumerate(pattern_x):
+        bag = []
 
-    # all stemmed words in THIS pattern
-    tokens = [stemmer.stem(w) for w in pattern]
+        # all stemmed words in THIS pattern
+        tokens = [stemmer.stem(w) for w in pattern]
 
-    # 1 = word exists, 0 = word does not exist in pattern
-    for w in words:
-        if w in tokens:
-            bag.append(1)
-        else:
-            bag.append(0)
+        # 1 = word exists, 0 = word does not exist in pattern
+        for w in words:
+            if w in tokens:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-    # copy over empty array size of labels
-    output_row = out_empty[:]
+        # copy over empty array size of labels
+        output_row = out_empty[:]
 
-    # ex.
-    # output row [index of (tag idx of current pattern)]
-    # tag_y[1] = Goodbye
-    # labels.index(goodbye) = 3 ; labels [greeting, anger, sad, goodbye]
-    # output_row[3] = 1
-    # this tells which tag correlates with pattern
-    output_row[labels.index(tag_y[i])] = 1
+        # ex.
+        # output row [index of (tag idx of current pattern)]
+        # tag_y[1] = Goodbye
+        # labels.index(goodbye) = 3 ; labels [greeting, anger, sad, goodbye]
+        # output_row[3] = 1
+        # this tells which tag correlates with pattern
+        output_row[labels.index(tag_y[i])] = 1
 
-    training.append(bag)
-    output.append(output_row)
+        training.append(bag)
+        output.append(output_row)
 
-training = np.array(training)
-output = np.array(output)
+    training = np.array(training)
+    output = np.array(output)
 ''' Preprocessing complete '''
 
 
